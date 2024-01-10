cf751602cd6973da9c3d8b4ed47ec0a7c52e5734
ieow <cwlee0@gmail.com>
2024-01-10 10:54:45 +0800

    Initial Commit

---
+++ Tests/mpc-kit-swiftTests/mpc_kit_swiftTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import mpc_kit_swift
+
+final class mpc_kit_swiftTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
