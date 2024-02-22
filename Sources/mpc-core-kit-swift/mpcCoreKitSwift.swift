122de60292ba0a0d2e2ea5431096038863e7349d
ieow <cwlee0@gmail.com>
2024-02-22 23:20:09 +0800

    fix: metadata pub

--- Sources/mpc-core-kit-swift/mpcCoreKitSwift.swift
+++ Sources/mpc-core-kit-swift/mpcCoreKitSwift.swift
@@ -210,7 +210,8 @@ public struct MpcCoreKit  {
             rss_comm: rss_comm)
 
         let key_details = try await thresholdKey.initialize(never_initialize_new_key: false, include_local_metadata_transitions: false)
-
+        self.appState.metadataPubKey = try key_details.pub_key.getPublicKey(format: .EllipticCompress)
+        
         self.tkey = thresholdKey
         
         if key_details.required_shares > 0 {
